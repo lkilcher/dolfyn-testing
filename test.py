8aca18525dd2a639c55ced8844b4bd97c9be9940
Levi Kilcher <levi.kilcher@gmail.com>
2021-12-19 16:21:04 -0700

    Figures for #8

--- test.py
+++ test.py
@@ -124,7 +124,7 @@ if False:
     axs[0].set_title("ACCLOW")
     fig.savefig('fig/' + fnm + '.ACCLOW.png')
     
-if True:
+if False:
     fnm = 'vector_data_imu01_rotate_earth2principal'
     # Compare the data that disagrees for this file:
     data0, data1 = compare_file(fnm + '.h5')
@@ -162,3 +162,48 @@ if True:
     axs[0].set_title("ANGRT")
     fig.savefig('fig/' + fnm + '.ANGRT.png')
     
+if True:
+    fnm = 'vector_data01'
+    # Compare the data that disagrees for this file:
+    data0, data1 = compare_file(fnm + '.h5')
+    # NOTE: THE DATA IN THESE FILES IS IDENTICAL!
+    
+    bnr1 = avm1.ADVBinner(20, fs=data1.attrs['fs'])
+    datb1 = bnr1(data1)
+
+    bnr0 = avm0.TurbBinner(20, fs=data0.props['fs'])
+    datb0 = bnr0(data0)
+
+    # Now do the calculations manually...
+    U = data0.vel[:,:80].reshape(3,4,20).copy()
+    u -= u.mean(-1)[:, :, None]
+    tke = (u ** 2).mean(-1)
+    stress = np.stack([u[0] * u[1], u[0] * u[2], u[1] * u[2]]).mean(-1)
+
+    fig1 = fig = plt.figure(500)
+    fig.clf()
+    fig, axs = plt.subplots(3, 1, num=fig.number, sharex=True, sharey=True)
+
+    for idx in range(3):
+        ax = axs[idx]
+        ax.plot(tke[idx], 'k--', lw=4, label='TRUTH')
+        ax.plot(datb0.tke_vec[idx], color='r', label='h5py')
+        ax.plot(datb1.tke_vec[idx], color='b', label='xarray')
+    ax.set_yscale('log')
+    ax.legend()
+        
+    axs[0].set_title("TKE")
+    fig.savefig('fig/' + fnm + '.TKE.png')
+
+    fig1 = fig = plt.figure(501)
+    fig.clf()
+    fig, axs = plt.subplots(3, 1, num=fig.number, sharex=True)
+    
+    for idx in range(3):
+        ax = axs[idx]
+        ax.plot(stress[idx], 'k--', lw=4, label='TRUTH')
+        ax.plot(datb0.stress[idx], color='r', label='h5py')
+        ax.plot(datb1.stress[idx], color='b', label='xarray')
+    axs[0].set_title("STRESS")
+    ax.legend()
+    fig.savefig('fig/' + fnm + '.STRESS.png')
